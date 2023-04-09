46290dc5187404f35a39148fb789e23cb693ec77
Filip <filipsarovic428@gmail.com>
2023-04-09 11:02:06 +0200

    Completed the first part of Footer implementation

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-
-import './Footer.css';
+import React from "react";
+import { FiFacebook, FiInstagram } from "react-icons/fi";
+// components
+import { FooterOverlay, Newsletter } from "../../components";
+// styles
+import "./Footer.css";
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
   </div>
 );
 
